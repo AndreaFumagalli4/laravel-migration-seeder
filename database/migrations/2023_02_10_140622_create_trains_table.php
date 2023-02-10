c3544d9ae7f84dcc6adb1a1c09421df003f8e483
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-02-10 15:25:10 +0100

    create tabel trains into db with the migration

---
+++ database/migrations/2023_02_10_140622_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('arrival_station');
+            $table->string('departure_station');
+            $table->date('time_of_departure');
+            $table->date('time_of_arrival');
+            $table->string('train_code');
+            $table->tinyInteger('number_of_coach');
+            $table->boolean('is_in_time');
+            $table->boolean('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
